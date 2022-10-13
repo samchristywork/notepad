f4873ab0bb6a873ab7c34dfb32452f7b9b35b581
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-10-12 20:45:36 -0500

    Added callback for when the user is typing.
    This callback is fired every time the user types, and currently prints
    out the contents of the text buffer.

--- notepad.c
+++ notepad.c
@@ -2,6 +2,22 @@
 
 GtkWidget *text_view;
 
+gboolean typingCallback(GtkWidget *widget, GdkEventKey *event,
+             gpointer data) {
+
+  GtkTextIter start;
+  GtkTextIter end;
+
+  GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+  gtk_text_buffer_get_start_iter(buffer, &start);
+  gtk_text_buffer_get_end_iter(buffer, &end);
+
+  char *str=gtk_text_buffer_get_text(buffer, &start, &end, 0);
+  printf("%s\n", str);
+
+  return FALSE;
+}
+
 gboolean keyPressCallback(GtkWidget *widget, GdkEventKey *event,
                           gpointer data) {
 
@@ -26,6 +42,9 @@ int main(int argc, char *argv[]) {
   gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
   g_signal_connect(G_OBJECT(window), "key_press_event",
                    G_CALLBACK(keyPressCallback), NULL);
+
+  g_signal_connect(G_OBJECT(text_view), "key_press_event",
+                   G_CALLBACK(typingCallback), NULL);
 
   gtk_widget_show_all(window);
   gtk_main();
