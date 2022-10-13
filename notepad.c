16472ddcf2b50d0ed1567af972162874637b07df
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-10-12 20:44:38 -0500

    Make the text view scrollable.
    This commit adds a container around the text view that is scrollable.

--- notepad.c
+++ notepad.c
@@ -17,8 +17,11 @@ int main(int argc, char *argv[]) {
 
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
+  GtkWidget *scrolled_window = gtk_scrolled_window_new(0, 0);
+  gtk_container_add(GTK_CONTAINER(window), scrolled_window);
+
   text_view = gtk_text_view_new();
-  gtk_container_add(GTK_CONTAINER(window), text_view);
+  gtk_container_add(GTK_CONTAINER(scrolled_window), text_view);
 
   gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
   g_signal_connect(G_OBJECT(window), "key_press_event",
