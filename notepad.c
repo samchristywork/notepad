f9e67735fb37a63e32d6cb95f631cd8148e90d97
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-10-15 19:38:18 -0500

    Add function call when opening a new file.

--- notepad.c
+++ notepad.c
@@ -33,6 +33,47 @@ gboolean keyPressCallback(GtkWidget *widget, GdkEventKey *event,
   return FALSE;
 }
 
+void open_file() {
+  GtkWidget *dialog = gtk_file_chooser_dialog_new("Open File",
+                                       NULL,
+                                       GTK_FILE_CHOOSER_ACTION_OPEN,
+                                       "_Cancel",
+                                       GTK_RESPONSE_CANCEL,
+                                       "_Open",
+                                       GTK_RESPONSE_ACCEPT,
+                                       NULL);
+
+  gint res = gtk_dialog_run(GTK_DIALOG(dialog));
+  if (res == GTK_RESPONSE_ACCEPT) {
+    char *filename;
+    GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+    filename = gtk_file_chooser_get_filename(chooser);
+
+    FILE *f = fopen(filename, "rb");
+    if (f) {
+      fseek(f, 0L, SEEK_END);
+      size_t len = ftell(f);
+      rewind(f);
+
+      char str[len + 1];
+      fread(str, 1, len, f);
+      str[len] = 0;
+      fclose(f);
+
+      //GtkTextIter iter;
+      GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+      //gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
+      //gtk_text_buffer_insert(buffer, &iter, str, -1);
+
+      gtk_text_buffer_set_text(buffer, str, strlen(str));
+    }
+
+    g_free(filename);
+  }
+
+  gtk_widget_destroy(dialog);
+}
+
 int main(int argc, char *argv[]) {
   gtk_init(&argc, &argv);
 
