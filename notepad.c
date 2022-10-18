60ccd8a6260b05912bbecc99ba4fc84c77fbe7c5
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-10-17 23:13:13 -0500

    Added save dialog box.
    The program can now read AND write...

--- notepad.c
+++ notepad.c
@@ -29,6 +29,38 @@ gboolean keyPressCallback(GtkWidget *widget, GdkEventKey *event, gpointer data) 
   return FALSE;
 }
 
+void save_file() {
+  GtkWidget *dialog = gtk_file_chooser_dialog_new("Save File",
+                                                  NULL,
+                                                  GTK_FILE_CHOOSER_ACTION_SAVE,
+                                                  "_Cancel",
+                                                  GTK_RESPONSE_CANCEL,
+                                                  "_Save",
+                                                  GTK_RESPONSE_ACCEPT,
+                                                  NULL);
+
+  gint res = gtk_dialog_run(GTK_DIALOG(dialog));
+  if (res == GTK_RESPONSE_ACCEPT) {
+    char *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+
+    FILE *f = fopen(filename, "wb");
+    if (f) {
+      GtkTextIter start;
+      GtkTextIter end;
+      GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+      gtk_text_buffer_get_start_iter(buffer, &start);
+      gtk_text_buffer_get_end_iter(buffer, &end);
+
+      char *str = gtk_text_buffer_get_text(buffer, &start, &end, 0);
+      fwrite(str, 1, strlen(str), f);
+    }
+
+    g_free(filename);
+  }
+
+  gtk_widget_destroy(dialog);
+}
+
 void open_file() {
   GtkWidget *dialog = gtk_file_chooser_dialog_new("Open File",
                                        NULL,
@@ -83,12 +115,14 @@ int main(int argc, char *argv[]) {
 
   GtkWidget *file = gtk_menu_item_new_with_label("File");
   GtkWidget *open = gtk_menu_item_new_with_label("Open");
+  GtkWidget *save = gtk_menu_item_new_with_label("Save");
   GtkWidget *quit = gtk_menu_item_new_with_label("Quit");
   GtkWidget *example = gtk_menu_item_new_with_label("Example");
 
   gtk_menu_item_set_submenu(GTK_MENU_ITEM(file), fileMenu);
   gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file);
   gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), open);
+  gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), save);
   gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), quit);
 
   gtk_menu_shell_append(GTK_MENU_SHELL(menubar), example);
@@ -125,6 +159,7 @@ int main(int argc, char *argv[]) {
   g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
   g_signal_connect(G_OBJECT(quit), "activate", G_CALLBACK(gtk_main_quit), NULL);
   g_signal_connect(G_OBJECT(open), "activate", G_CALLBACK(open_file), NULL);
+  g_signal_connect(G_OBJECT(save), "activate", G_CALLBACK(save_file), NULL);
 
   gtk_widget_show_all(window);
   gtk_main();
